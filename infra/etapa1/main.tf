f2773b0ed74cc46c8304f4584a6833b779c37603
Ricardo Novoa Ll. <ri.novoa@duocuc.cl>
2026-05-16 20:37:50 -0400

    Arregle el main de la etapa 1 para que coincida con el backend

--- infra/etapa1/main.tf
+++ infra/etapa1/main.tf
@@ -11,14 +11,20 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Creamos el ECR para el Backend
-resource "aws_ecr_repository" "backend" {
-  name = "${var.nombre_proyecto}-backend"
-  force_delete = true
-}
-
 # Creamos el ECR para el Frontend
 resource "aws_ecr_repository" "frontend" {
   name = "${var.nombre_proyecto}-frontend"
+  force_delete = true
+}
+
+# Creamos el ECR para el Back Ventas
+resource "aws_ecr_repository" "back_ventas" {
+  name = "${var.nombre_proyecto}-back-ventas"
+  force_delete = true
+}
+
+# Creamos el ECR para el Back Despachos
+resource "aws_ecr_repository" "back_despachos" {
+  name = "${var.nombre_proyecto}-back-despachos"
   force_delete = true
 }
